e5ad9657d990bc7542aad3fe69503e2730223142
rana1365 <rana135105@gmail.com>
2025-04-06 01:28:31 +0200

    Frontent Setup and Hero Section implement

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Shop from './components/Shop'
+
+function App() {
+
+  return (
+    <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/shop' element={<Shop/>}/>
+        </Routes>
+      </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App

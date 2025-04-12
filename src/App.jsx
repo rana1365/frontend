a3fd93a8a6a9d058f132424811c642430a0faad6
rana1365 <rana135105@gmail.com>
2025-04-12 05:51:18 +0200

    Nostate, Loader, Category insert api integration and Category showing complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Shop from './components/Shop'
@@ -9,6 +9,8 @@ import Login from './components/admin/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import Dashboard from './components/admin/Dashboard'
 import { AdminRequireAuth } from './components/admin/AdminRequireAuth'
+import { default as ShowCategories } from './components/admin/category/Show'
+import { default as CreateCategory } from './components/admin/category/Create'
 
 function App() {
 
@@ -22,11 +24,25 @@ function App() {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/checkout' element={<Checkout/>}/>
           <Route path='/admin/login' element={<Login/>}/>
+
           <Route path='/admin/dashboard' element={
             <AdminRequireAuth>
                 <Dashboard/>
             </AdminRequireAuth>
           }/>
+
+          <Route path='/admin/categories' element={
+            <AdminRequireAuth>
+                <ShowCategories/>
+            </AdminRequireAuth>
+          }/>
+
+          <Route path='/admin/categories/create' element={
+            <AdminRequireAuth>
+                <CreateCategory/>
+            </AdminRequireAuth>
+          }/>
+
         </Routes>
       </BrowserRouter>
       <ToastContainer />
